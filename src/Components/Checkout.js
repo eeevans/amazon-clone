2454808a65b047b598282baff7eb74c3d845b488
eeevans <ed.eevans@gmail>
2020-08-08 01:26:00 -0500

    added subtotal

--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -2,33 +2,41 @@ import React from 'react';
 import "./Checkout.css"
 import CheckoutProduct from "./CheckoutProduct"
 import { useStateValue } from "./StateProvider";
+import Subtotal from "./Subtotal"
 
 function Checkout() {
-    const [{basket}] = useStateValue();
+    const [{ basket }] = useStateValue();
     return (
-        <div>
-            <img className="checkout__ad" 
-            src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" 
-            alt="" />
+        <div className="checkout">
+            <div className="checkout__left" >
+                <img className="checkout__ad"
+                    src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+                    alt="" />
 
-            {basket?.length === 0 ? (
-                <div>
-                    <h2>Your shopping basket is empty.</h2>
+                {basket?.length === 0 ? (
+                    <div>
+                        <h2>Your shopping basket is empty.</h2>
                     </div>
-            ) : (
-                <div>
-                    <h2>Your shopping basket</h2>
+                ) : (
+                        <div>
+                            <h2>Your shopping basket</h2>
 
-                    {basket.map(item => (
-                        <CheckoutProduct 
-                            id={item.id}
-                            title={item.title}
-                            image={item.image}
-                            price={item.price}
-                            rating={item.rating}
-                            key={item.id}
-                        />
-                    ))}
+                            {basket.map(item => (
+                                <CheckoutProduct
+                                    id={item.id}
+                                    title={item.title}
+                                    image={item.image}
+                                    price={item.price}
+                                    rating={item.rating}
+                                    key={item.id}
+                                />
+                            ))}
+                        </div>
+                    )}
+            </div>
+            {basket.length > 0 && (
+                <div className="checkout__right" >
+                    <Subtotal />
                 </div>
             )}
         </div>
